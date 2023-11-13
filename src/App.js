923b1499577a8be4fa69b41bd5b9bad1003741b0
md niyaz <mdniyaz1842000@gmail.com>
2023-11-13 15:50:17 +0530

    refector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css"
 import Mainroute from "../src/Routes/Index"
-import Crousal1 from "./components/UI-components/Crousal1";
-import Crousel from "./components/UI-components/Crousel";
+
 // import Fontex from "./Fontex";
 
 
